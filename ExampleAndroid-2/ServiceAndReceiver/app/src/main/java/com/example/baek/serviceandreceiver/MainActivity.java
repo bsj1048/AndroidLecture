81c00c49cabd88ce5c865385dc3fd3763f54ad98
빽님 <bsj1048@cnu.ac.kr>
2018-10-12 14:45:09 +0900

    add service and broadcast receiver

--- ExampleAndroid-2/ServiceAndReceiver/app/src/main/java/com/example/baek/serviceandreceiver/MainActivity.java
+++ ExampleAndroid-2/ServiceAndReceiver/app/src/main/java/com/example/baek/serviceandreceiver/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.baek.serviceandreceiver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button0;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0  = (Button) findViewById(R.id.button0);
+        button1  = (Button) findViewById(R.id.button1);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SampleService.class);
+                intent.putExtra("command", "show");
+                intent.putExtra("name", "백승진");
+                startService(intent);
+            }
+        });
+
+        Intent passedIntent = getIntent();
+        processCommand(passedIntent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        processCommand(intent);
+    }
+
+    private void processCommand(Intent intent) {
+        if(intent != null) {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+
+            Toast.makeText(this, command + ", " + name, Toast.LENGTH_LONG).show();
+        }
     }
 }
